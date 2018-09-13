84b830dd96055c802d81f3d679b6e26824439cb7
Astrid Varga <astrid.varga@gmx.de>
2018-09-13 09:47:52 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        // let hello = "Hello World"
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <h2>0</h2>
+                <button>+</button>
+                <button>-</button>
+            </div>
+        );
+    }
+}
+
+export default App;
